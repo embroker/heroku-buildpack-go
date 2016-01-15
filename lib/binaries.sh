52949de1b6e5e7e20c3e1ccd0a10e27cfdc72ab8
Bojan Petrovic <bojan@embroker.com>
2016-01-15 15:34:31 +0100

    Update

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -1,3 +1,12 @@
+needs_resolution() {
+  local semver=$1
+  if ! [[ "$semver" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
 install_nodejs() {
   local version="0.10.25"
   local dir="$1"
